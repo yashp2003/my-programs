bf7dc853808cdfebbd56ec59bc05ef0cefddb505
Yash Bijesh Patel <106303528+yashp2003@users.noreply.github.com>
2022-05-26 14:46:16 +0530

    Use of comments

---
+++ comments.py
@@ -0,0 +1,6 @@
+# Hello, I am yash and I will comment out this line using "Ctrl + /"
+import time
+time.sleep(1)
+print("This string will print after 1 sec")
+'''Feel free to change 
+the time interval'''
