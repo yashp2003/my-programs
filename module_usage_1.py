96d44bed304da6303289692f14214c62ce6b3781
Yash Bijesh Patel <106303528+yashp2003@users.noreply.github.com>
2022-05-26 14:27:22 +0530

    Use of module
    A code with a use of module. 26/05/22. In this code I've used "Time" module and this program will print the respective string after 5 sec.

---
+++ module_usage_1.py
@@ -0,0 +1,4 @@
+# Usage of module. Code with the use of module. Here I have used "time" module. And this code will print the respective string after 5 sec. 
+import time
+time.sleep(5)
+print("This string will print after 5 sec")
