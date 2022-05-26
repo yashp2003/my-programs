fec64fc70c671f6c14a333eb740d755a0aafd260
Yash Bijesh Patel <106303528+yashp2003@users.noreply.github.com>
2022-05-26 14:56:46 +0530

    Use of print function.
    Code to demonstrate the use of print function. 26/05/22. In this program I've print a poem as it is using print function.

---
+++ 02_pr_01_twinkle.py
@@ -0,0 +1,5 @@
+# Code to print the 'Twinkle-Twinkle' poem as it it.(pr)
+print ('''Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky.''')
