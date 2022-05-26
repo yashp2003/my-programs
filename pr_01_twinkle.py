828f158714c47b2c2a70396bee6bf9e4a82e20eb
Yash Bijesh Patel <106303528+yashp2003@users.noreply.github.com>
2022-05-26 15:13:03 +0530

    Use of print function
    Code to demonstrate the use of print function. 26/05/22. Here I've used different approaches to print the same string.

--- pr_01_twinkle.py
+++ pr_01_twinkle.py
@@ -4,3 +4,30 @@ How I wonder what you are
 Up above the world so high
 Like a diamond in the sky.''')
 
+# Use "Alt + shift" to use multi-functionalty at one (vs-code feature)
+print("Twinkle, twinkle, little star")
+print("How I wonder what you are")
+print("Up above the world so high")
+print("Like a diamond in the sky")
+print("Twinkle, twinkle, little star")
+print("How I wonder what you are")
+print("Twinkle, twinkle, little star")
+print("How I wonder what you are")
+print("Up above the world so high")
+print("Like a diamond in the sky")
+print("Twinkle, twinkle, little star")
+print("How I wonder what you are")
+
+# Another approach
+print('''Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are''')
