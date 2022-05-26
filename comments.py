f9b8f8f78f8f1ff72ef45cb7d75dc96a3eec29a3
Yash Bijesh Patel <106303528+yashp2003@users.noreply.github.com>
2022-05-26 14:50:41 +0530

    Use of comments
    This program demonstrate the use of "comments". 26/05/22. Here I've used 'single' and 'multi-line' comments.

--- comments.py
+++ comments.py
@@ -1,6 +1,8 @@
+# Code to demonstrate the use of comments.
 # Hello, I am yash and I will comment out this line using "Ctrl + /"
 import time
 time.sleep(1)
 print("This string will print after 1 sec")
 '''Feel free to change 
 the time interval'''
+
